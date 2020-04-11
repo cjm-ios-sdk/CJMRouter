fed7b1504e35802aaf62b5f7d59dbc2c07283b2d
chenjm <cjiemin@163.com>
2020-04-11 20:51:05 +0800

    【bug fixed】缺少import UIKit 导致应用的项目编译报错。

--- CJMRouter/Classes/CJMRouterErrorViewController.h
+++ CJMRouter/Classes/CJMRouterErrorViewController.h
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 @interface CJMRouterErrorViewController : UIViewController
 @property (nonatomic, copy) NSString *_Nullable errorMessage;
